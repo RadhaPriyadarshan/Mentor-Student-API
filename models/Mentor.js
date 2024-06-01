5d2b0b6101a06360f6f413126f4581c869dd7237
Radha priyadarsan <radhapriyan786@gmail.com>
2024-06-02 01:29:27 +0530

    update commit

--- models/Mentor.js
+++ models/Mentor.js
@@ -1,19 +1,20 @@
 const mongoose = require('mongoose');
 
 const mentorSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    students: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Student'
-    }],
-    mentor_id: {
-        type: String,
-        required: true,
-        unique: true
-    }
+  mentor_id: { type: Number, unique: true },
+  name: String,
+  // other mentor properties
+});
+
+mentorSchema.pre('save', async function(next) {
+  try {
+    if (!this.isNew) return next(); // If not a new document, do nothing
+    const lastMentor = await Mentor.findOne().sort({ mentor_id: -1 });
+    this.mentor_id = lastMentor ? lastMentor.mentor_id + 1 : 1;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const Mentor = mongoose.model('Mentor', mentorSchema);
--- models/Student.js
+++ models/Student.js
@@ -1,25 +1,21 @@
 const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    mentor: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Mentor',
-        default: null
-    },
-    previousMentor: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Mentor',
-        default: null
-    },
-    student_id: {
-        type: String,
-        required: true,
-        unique: true
-    }
+  student_id: { type: Number, unique: true },
+  name: String,
+  mentor: { type: mongoose.Schema.Types.ObjectId, ref: 'Mentor' },
+  // other student properties
+});
+
+studentSchema.pre('save', async function(next) {
+  try {
+    if (!this.isNew) return next(); // If not a new document, do nothing
+    const lastStudent = await Student.findOne().sort({ student_id: -1 });
+    this.student_id = lastStudent ? lastStudent.student_id + 1 : 1;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const Student = mongoose.model('Student', studentSchema);
